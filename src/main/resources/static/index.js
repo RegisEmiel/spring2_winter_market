d8e2f4924098605dcda523dbbd8d598d7bf3d425
RegisEmiel <regisemiel@yandex.ru>
2022-02-17 21:51:02 +0300

    Initial commit

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,21 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    $scope.loadProducts = function () {
+        $http.get('http://localhost:8189/winter/api/v1/products').then(function (response) {
+            $scope.productsList = response.data;
+        });
+    }
+
+    $scope.showProductInfo = function (productId) {
+        $http.get('http://localhost:8189/winter/api/v1/products/' + productId).then(function (response) {
+            alert(response.data.title);
+        });
+    }
+
+    $scope.deleteProductById = function (productId) {
+        $http.delete('http://localhost:8189/winter/api/v1/products/' + productId).then(function (response) {
+            $scope.loadProducts();
+        });
+    }
+
+    $scope.loadProducts();
+});
